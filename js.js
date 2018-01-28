c7d7c38c66dff03c63e6c293251a0800a4bf923d
Jonas Wirth <jonas-wirth@gmx.ch>
2018-01-28 20:42:12 +0100

    Completed

--- js.js
+++ js.js
@@ -1,3 +1,5 @@
+const dayMs = 86400000;
+
 var rsStart = new Date(2018, 0, 15);
 var rsEnd = new Date(2018, 4, 18);
 
@@ -10,8 +12,20 @@ var progressRs = 100 / totalRs * passedRs;
 function setProgressbar(id, progress){
   var elem = document.getElementById(id);
   var elemProgress = elem.childNodes[1];
-  console.log(elem.childNodes);
-  elemProgress.style.width = progress + "%";
+  var elemPercent = elem.childNodes[3];
+  var elemDaysLeft = elem.childNodes[5];
+
+  if(progress < 100)
+  {
+    elemProgress.style.width = progress + "%";
+    elemPercent.innerHTML = (Math.round(progress * 100) / 100) + " %";
+    elemDaysLeft.innerHTML = "noch " + Math.round((totalRs - passedRs) / dayMs) + " Tage";
+  }
+  else{
+    elemProgress.style.width = "100%";
+    elemPercent.innerHTML = "100 %";
+    elemDaysLeft.innerHTML = "FERTIG!";
+  }
 }
 
 document.onreadystatechange = () => {
